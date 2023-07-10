65f2571e7e61dd29cdf96a044deba356410d917e
fredegd <124848643+fredegd@users.noreply.github.com>
2023-07-10 14:44:02 +0200

    image upload Frontend

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,82 +1,126 @@
 import { useState } from "react";
+import axios, { Axios } from "axios";
+import { useForm } from "react-hook-form";
 
 const Upload = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [information, setInformation] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-const [selectedImage, setSelectedImage] = useState(null);
-const [information, setInformation] = useState('');
-const [selectedCategory, setSelectedCategory] = useState('');
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-const handleImageChange = (e) => {
-	const file = e.target.files[0];
-	setSelectedImage(file);
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    formData.append("information", data.information);
+    formData.append("category", data.category);
+
+    axios.post("http://localhost:3000/images", formData)
+			.then((res)=>{
+				console.log(res.data)
+			})
+			.catch((err)=>{
+				console.log(err)
+			});
+			console.log(data)
+  };
+
+  // const handleImageChange = (e) => {
+  // 	const file = e.target.files[0];
+  // 	setSelectedImage(file);
+  // };
+
+  // const handleInformationChange = (e) => {
+  // 	setInformation(e.target.value);
+  // };
+
+  // const handleCategoryChange = (e) => {
+  // 	setSelectedCategory(e.target.value);
+  // };
+
+  // const handleSubmit = (e) => {
+  // 	e.preventDefault();
+  // 	setSelectedImage(null);
+  // 	setInformation('');
+  // 	setSelectedCategory('');
+  // 	console.log(e)
+  // };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col justify-center items-center"
+    >
+      <div>
+        <label htmlFor="image" className="block mb-2">
+          Select Image:
+        </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*"
+          {...register("image", { required: true })}
+          // onChange={handleImageChange}
+          className="border border-gray-300 px-4 py-2 rounded-md w-80"
+        />
+      </div>
+      <div className="mt-4">
+        <label htmlFor="information" className="block mb-2">
+          Information:
+        </label>
+        <input
+          type="text"
+          id="information"
+          {...register("information", { required: true })}
+          //   value={information}
+          //   onChange={handleInformationChange}
+          className="border border-gray-300 px-4 py-2 rounded-md w-80"
+        />
+      </div>
+     
+	 <div className="mt-4">
+        <label htmlFor="category" className="block mb-2">
+          Select Category:
+        </label>
+        <select
+          id="category"
+        //   value={selectedCategory}
+          {...register("category", { required: true })}
+          //   onChange={handleCategoryChange}
+          className="border border-gray-300 px-4 py-2 rounded-md w-80"
+        >
+          <option className="text-slate" value="">
+            Select a category
+          </option>
+          <option className="text-slate" value="animals">
+            Animals
+          </option>
+          <option className="text-slate" value="vacation">
+            Vacation
+          </option>
+          <option className="text-slate" value="party">
+            Party
+          </option>
+          <option className="text-slate" value="family">
+            Family
+          </option>
+        </select>
+      </div>
+      <div className="mt-4">
+        <button
+          type="submit"
+          className="bg-blue-800 text-white px-4 py-2 w-80 rounded-md"
+        >
+          Upload
+        </button>
+      </div>
+    </form>
+  );
 };
 
-const handleInformationChange = (e) => {
-	setInformation(e.target.value);
-};
-
-const handleCategoryChange = (e) => {
-	setSelectedCategory(e.target.value);
-};
-
-const handleSubmit = (e) => {
-	e.preventDefault();
-	setSelectedImage(null);
-	setInformation('');
-	setSelectedCategory('');
-};
-
-return (
-<form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
-	<div>
-		<label htmlFor="image" className="block mb-2">
-			Select Image:
-		</label>
-		<input
-			type="file"
-			id="image"
-			accept="image/*"
-			onChange={handleImageChange}
-			className="border border-gray-300 px-4 py-2 rounded-md w-80"
-		/>
-	</div>
-	<div className="mt-4">
-		<label htmlFor="information" className="block mb-2">
-		Information:
-		</label>
-		<input
-			type="text"
-			id="information"
-			value={information}
-			onChange={handleInformationChange}
-			className="border border-gray-300 px-4 py-2 rounded-md w-80"/>
-	</div>
-	<div className="mt-4">
-		<label htmlFor="category" className="block mb-2">
-		Select Category:
-		</label>
-		<select
-			id="category"
-			value={selectedCategory}
-			onChange={handleCategoryChange}
-			className="border border-gray-300 px-4 py-2 rounded-md w-80"
-		>
-			<option  className="text-slate"  value="">Select a category</option>
-			<option  className="text-slate"  value="category1">Animals</option>
-			<option  className="text-slate"  value="category2">Vacation</option>
-			<option  className="text-slate"  value="category3">Party</option>
-			<option  className="text-slate"  value="category4">Family</option>
-		</select>
-	</div>
-	<div className="mt-4">
-		<button
-			type="submit"
-			className="bg-blue-800 text-white px-4 py-2 w-80 rounded-md">
-		Upload
-		</button>
-	</div>
-</form>
-	);
-}
-
-export default Upload
+export default Upload;
