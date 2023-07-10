0532e6e98983f4ee69ceed806bff9bd78c246266
claudia-sikorski <c.sikorski@outlook.de>
2023-07-10 15:47:09 +0200

    add home inclusive style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,22 @@
+import homeStageImage from "../assets/home-stage-image.jpg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Album Booth</h1>
-      <p>Collect Moments, not Things</p>
+    <div
+      className="h-screen bg-center flex justify-center items-center"
+      style={{ backgroundImage: `url(${homeStageImage})` }}
+    >
+      <div className="flex-col justify-center items-center bg-gray-300 bg-opacity-80">
+        <h1 className="flex justify-center text-5xl px-10 mt-5">Album Booth</h1>
+        <p className="flex justify-center mt-5 text-xl italic">
+          Collect Moments, not Things!
+        </p>
+        <a href="/upload" className="flex justify-center mt-3">
+          <button className="bg-blue-900 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full mb-5">
+            Start now
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
